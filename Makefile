0f8759cf886dda4a8eec0fe9cef63b38ceac89db
alexcreek <me@alexcreek.com>
2017-12-22 05:53:46 +0000

    init

---
+++ Makefile
@@ -0,0 +1,2 @@
+start:
+	export FLASK_DEBUG=0 FLASK_APP=remote.py; flask run -h 0.0.0.0 -p 5000
---
+++ remote.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+import time
+
+app = Flask(__name__)
+movie_dir = '/data/rutorrent/incoming'
+
+@app.route('/')
+def index():
+    movies = [x for x in sorted(os.listdir('/data/rutorrent/incoming')) if 'EVO' in x]
+
+    foo = [(x[0], time.ctime(x[1].st_ctime)) for x in sorted([(fn, os.stat(movie_dir + '/' + fn)) for fn in movies], key = lambda x: x[1].st_ctime, reverse=True)]
+    bar = [x[0] for x in foo]
+
+    return render_template('index.html', movies=bar)
+
