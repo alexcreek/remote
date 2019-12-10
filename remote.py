fa50a066234c51f240964e59ea8fdbd794a06fd3
Alex Creek <me@alexcreek.com>
2019-12-10 00:23:40 -0500

    remove filter

--- remote.py
+++ remote.py
@@ -7,7 +7,7 @@ movie_dir = '/data/rutorrent/incoming'
 
 @app.route('/')
 def index():
-    movies = [x for x in sorted(os.listdir('/data/rutorrent/incoming')) if 'EVO' in x]
+    movies = [x for x in sorted(os.listdir('/data/rutorrent/incoming'))]
 
     foo = [(x[0], time.ctime(x[1].st_ctime)) for x in sorted([(fn, os.stat(movie_dir + '/' + fn)) for fn in movies], key = lambda x: x[1].st_ctime, reverse=True)]
     bar = [x[0] for x in foo]
