6290c46b02eca55ad2a2fe2254592adca7658d52
alexcreek <me@alexcreek.com>
2020-05-03 23:02:57 -0400

    use parameterized queries

--- remote/db.py
+++ remote/db.py
@@ -5,28 +5,29 @@ class Db():
     def __init__(self):
         DB_DIR = os.getenv('DB_DIR', '/var/lib/remote')
         self.conn = sqlite3.connect('{}/remote.db'.format(DB_DIR))
+        self._initdb()
 
-    def initdb(self):
+    def _initdb(self):
         schema = '''CREATE TABLE IF NOT EXISTS movie (
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL UNIQUE,
-            path TEXT NOT NULL,
+            year TEXT NOT NULL,
             overview TEXT NOT NULL,
-            genre TEXT NOT NULL,
+            path TEXT NOT NULL,
             poster_url TEXT NOT NULL
             );'''
         c = self.conn.cursor()
         c.execute(schema)
         self.conn.commit()
 
-    def insert(self, sql):
+    def insert(self, sql, *values):
         c = self.conn.cursor()
-        c.execute(sql)
+        c.executemany(sql, [values])
         self.conn.commit()
 
-    def delete(self, sql):
+    def delete(self, sql, *values):
         c = self.conn.cursor()
-        c.execute(sql)
+        c.executemany(sql, [values])
         self.conn.commit()
 
     def query(self, sql):
